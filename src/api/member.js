5822c40f657f0cbab82b54b53adb2faf86541167
xiaoxiaoweii <2679656468@qq.com>
2019-12-19 17:38:49 +0800

    会员管理

---
+++ src/api/member.js
@@ -0,0 +1,49 @@
+import request from "@/utils/request";
+
+export default {
+  //获取会员列表数据
+  getList() {
+    return request({
+      url: '/member/list',
+      method: 'get',
+    })
+  },
+  //分页搜索方法
+  search(page, size, searchMap) {
+    return request({
+      url: `/member/list/search/$(page)/$(size)`,
+      method: 'post',
+      data: searchMap
+    })
+  },
+  // 新增会员数据
+  add(pojo) {
+    return request({
+      url: `/member`,
+      method: 'post',
+      data: pojo
+    })
+  },
+  // 通过id查询
+  getById(id) {
+    return request({
+      url: `/member//${id}`,
+      method: 'get',
+    })
+  },
+  //更新数据
+  update(pojo) {
+    return request({
+      url: `/member/${pojo.id}`,
+      method: 'put',
+      data: pojo
+    })
+  },
+  //删除用户
+  deleteById(id) {
+    return request({
+      url: `/member/${id}`,
+      method: 'delete'
+    })
+  }
+}
--- src/views/member/index.vue
+++ src/views/member/index.vue
@@ -1,5 +1,310 @@
 <template>
   <div>
-    会员
+    <!-- 搜索 -->
+    <el-form ref="searchForm" :inline="true" :model="searchMap" style="margin-top: 20px;">
+      <el-form-item prop="cardNum">
+        <el-input v-model="searchMap.cardNum" placeholder="会员卡号"></el-input>
+      </el-form-item>
+      <el-form-item prop="name">
+        <el-input v-model="searchMap.name" placeholder="会员名字"></el-input>
+      </el-form-item>
+      <el-form-item prop="payType">
+        <el-select v-model="searchMap.payType" placeholder="支付类型">
+          <el-option
+            v-for="option in payTypeOptions"
+            :key="option.name"
+            :label="option.name"
+            :value="option.type"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item prop="birthday">
+        <el-date-picker
+          value-format="yyyy-MM-dd"
+          v-model="searchMap.birthday"
+          type="date"
+          placeholder="出生日期"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="fetchData">查询</el-button>
+        <el-button type="primary" @click="handleAdd">新增</el-button>
+        <el-button type="primary" @click="resetForm('searchForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 数据列表 -->
+    <!-- ：data 绑定渲染的数据  -->
+    <!-- border 表格边框 -->
+    <el-table :data="list" style="width: 100%" height="600" border>
+      <el-table-column fixed type="index" label="序号"></el-table-column>
+      <el-table-column fixed prop="cardNum" label="会员卡号"></el-table-column>
+      <el-table-column fixed prop="name" label="会员姓名"></el-table-column>
+      <el-table-column fixed prop="birthday" label="会员生日"></el-table-column>
+      <el-table-column fixed prop="phone" label="手机号码"></el-table-column>
+      <el-table-column fixed prop="integral" label="可用积分"></el-table-column>
+      <el-table-column fixed prop="money" label="开卡金额"></el-table-column>
+      <el-table-column fixed prop="payType" label="支付类型">
+        <template slot-scope="scope">
+          <span>{{scope.row.payType | payTypeFilter}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column fixed prop="address" label="会员地址"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.row.id)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDele(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页组件 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="10"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
+    <!-- 弹出新增窗口 -->
+    <el-dialog title="会员编辑" :visible.sync="dialogFormVisible" width="500px">
+      <el-form
+        :rules="rules"
+        :model="pojo"
+        ref="pojoForm"
+        label-width="100px"
+        label-position="right"
+        style="width: 400px"
+      >
+        <el-form-item label="会员卡号" prop="cardNum">
+          <el-input v-model="pojo.cardNum"></el-input>
+        </el-form-item>
+        <el-form-item label="会员姓名" prop="name">
+          <el-input v-model="pojo.name"></el-input>
+        </el-form-item>
+        <el-form-item label="会员生日" prop="birthday">
+          <el-date-picker
+            value-format="yyyy-MM-dd"
+            v-model="pojo.birthday"
+            type="date"
+            placeholder="会员生日"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item label="手机号码" prop="phone">
+          <el-input v-model="pojo.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="开卡金额" prop="money">
+          <el-input v-model="pojo.money"></el-input>
+        </el-form-item>
+        <el-form-item label="可用积分" prop="integral">
+          <el-input v-model="pojo.integral"></el-input>
+        </el-form-item>
+        <el-form-item label="支付类型" prop="payType">
+          <el-select v-model="pojo.payType" placeholder="支付类型">
+            <el-option
+              v-for="option in payTypeOptions"
+              :key="option.name"
+              :label="option.name"
+              :value="option.type"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="会员地址" prop="address">
+          <el-input v-model="pojo.address" type="textarea"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="pojo.id === null ? addData('pojoForm'): updateData('pojoForm')"
+        >确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
+
+<script>
+import memberApi from "@/api/member";
+
+// 支付类型
+const payTypeOptions = [
+  { type: "1", name: "现金" },
+  { type: "2", name: "微信" },
+  { type: "3", name: "支付宝" },
+  { type: "3", name: "银联" }
+];
+
+export default {
+  data() {
+    return {
+      list: [],
+      total: 0,
+      currentPage: 1,
+      pageSize: 10,
+      searchMap: {
+        cardNum: "",
+        name: "",
+        payType: "",
+        birthday: ""
+      },
+      payTypeOptions,
+      dialogFormVisible: false, //控制对话框
+      pojo: {
+        id: null,
+        cardNum: "",
+        name: "",
+        birthday: "",
+        phone: "",
+        money: 0,
+        integral: 0,
+        payType: "",
+        address: ""
+        // integral: 0
+      }, //提交的数据
+      rules: {
+        //校验规则
+        cardNum: [{ required: true, message: "卡号不能为空", trigger: "blur" }],
+        name: [{ required: true, message: "姓名不能为空", trigger: "blur" }],
+        payType: [
+          { required: true, message: "支付类型不能为空", trigger: "change" }
+        ]
+      }
+    };
+  },
+  created() {
+    //初始化获取列表数据
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      memberApi
+        .search(this.currentPage, this.pageSize, this.searchMap)
+        .then(response => {
+          // memberApi.getList().then(response => {
+          //调用分页查询数据
+          const resp = response.data;
+          this.list = resp.data.rows;
+          this.total = resp.data.total;
+          // console.log(resp.data.rows);
+        });
+    },
+    //当每页条数改变触发
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.fetchData();
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      this.fetchData();
+    },
+    // 重置按钮
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    // 提交新增数据
+    addData(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          //提交表单
+          memberApi.add(this.pojo).then(response => {
+            const resp = response.data;
+            if (resp.flag) {
+              //新增成功， 刷新列表数据
+              this.fetchData();
+              this.dialogFormVisible = false; //关闭窗口
+            } else {
+              // 失败的提示信息
+              this.$message({
+                message: resp.message,
+                type: "warning"
+              });
+            }
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    //弹出新增窗口
+    handleAdd() {
+      this.dialogFormVisible = true;
+      this.$nextTick(() => {
+        //当渲染结束  dom执行
+        //弹出窗口打开后 需要加载dom 需要花费时间  再调用resetFields 重置表单和请清楚样式
+        this.$refs["pojoForm"].resetFields();
+      });
+    },
+    //打开编辑窗口
+    handleEdit(id) {
+      // 清空原数据
+      this.handleAdd();
+      // 通过Id查询数据
+      memberApi.getById(id).then(response => {
+        const resp = response.data;
+        if (resp.flag) {
+          this.pojo = resp.data;
+        }
+      });
+    },
+    //更新数据
+    updateData(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          // 验证通过，提交
+          memberApi.update(this.pojo).then(response => {
+            const resp = response.data;
+            if (resp.flag) {
+              this.fetchData(); // 刷新列表数据
+              this.dialogFormVisible = false; // 关闭窗口
+            } else {
+              // 失败, 弹出提示
+              this.$message({
+                message: resp.message,
+                type: "warning"
+              });
+            }
+          });
+        } else {
+          // 验证不通过
+          return false;
+        }
+      });
+    },
+    handleDele(id) {
+      console.log('删除', id)
+      this.$confirm('确认删除这条记录吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 确认
+        memberApi.deleteById(id).then(response => {
+          const resp = response.data
+          //提示信息
+          this.$message({
+            type: resp.flag ? 'success':'error',
+            message: resp.message
+          });
+          if(resp.flag) {
+            // 删除成功，刷新列表
+            this.fetchData()
+          }
+        })
+      }).catch(() => {
+        // 取消删除，不理会
+      })
+    }
+  },
+  filters: {
+    payTypeFilter(type) {
+      // payTypeOptions.find(obj => {
+      //   obj.type === type
+      // })
+      // 在过滤器中不能引用当前实例
+      const payObj = payTypeOptions.find(obj => obj.type === type);
+      return payObj ? payObj.name : null;
+    }
+  }
+};
+</script>
