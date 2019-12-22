6734d96b2c7adbe21f896edfc8919648f2b2b850
xiaoxiaoweii <2679656468@qq.com>
2019-12-22 16:42:35 +0800

    修改密码对话框实现 未自定义校验和修改密码业务逻辑

--- src/components/AppHeader/index.vue
+++ src/components/AppHeader/index.vue
@@ -17,45 +17,110 @@
         <el-dropdown-item icon="el-icon-close" command="b">退出系统</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
+    <!-- 修改密码 -->
+    <el-dialog title="修改密码" :visible.sync="dialogFormVisible" width="400px">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        style="width: 300px;"
+      >
+        <el-form-item label="原密码" prop="oldPass">
+          <el-input type="password" v-model="ruleForm.oldPass"></el-input>
+        </el-form-item>
+        <el-form-item label="新密码" prop="pass">
+          <el-input type="password" v-model="ruleForm.pass"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input type="password" v-model="ruleForm.checkPass"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+          <el-button @click="$refs['ruleForm'].resetFields()">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import {logout} from '@/api/login';
+import { logout } from "@/api/login";
 export default {
+  data() {
+    return {
+      dialogFormVisible: false,
+      ruleForm: {
+        oldPass: '',
+        pass: '',
+        checkPass: ''
+      },
+      rules: {
+        oldPass: [
+          {required: true, message: '原密码不能为空', trigger:"blur"}
+        ],
+        pass: [
+          {required: true, message: '请输入新密码', trigger:"blur"}
+        ],
+        checkPass: [
+          {required: true, message: '请确认密码', trigger:"blur"}
+        ]
+      }   
+    }
+  },
   methods: {
     handleCommand(command) {
       switch (command) {
         case "a":
-          // 修改密码
-          this.$message("点击修改密码");
+          // 打开修改密码
+          this.handlePwd()
           break;
         case "b":
-          // 退出系统
-          logout(localStorage.getItem('vue_system-token')).then(response => {
-            const resp = response.data
-            if(resp.flag) {
-              //退出成功
-              //清楚本地数据
-              localStorage.removeItem('vue_system-user')
-              localStorage.removeItem('vue_system-token')
-              //ui到登陆界面
-              this.$router.push('/login')
-            }else {
-              this.$message({
-                message: resp.message,
-                token: 'warning',
-                duration: 500 //弹出时间
-              })
-            }
-          })
+          this.handleLogout();
           break;
         default:
           break;
       }
+    },
+    // 退出系统
+    handleLogout() {
+      logout(localStorage.getItem("vue_system-token")).then(response => {
+        const resp = response.data;
+        if (resp.flag) {
+          //退出成功
+          //清楚本地数据
+          localStorage.removeItem("vue_system-user");
+          localStorage.removeItem("vue_system-token");
+          //ui到登陆界面
+          this.$router.push("/login");
+        } else {
+          this.$message({
+            message: resp.message,
+            token: "warning",
+            duration: 500 //弹出时间
+          })
+        }
+      })
+    },
+    // 打开修改密码窗口
+    handlePwd() {
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['ruleForm'].resetFields()
+      })
+    },
+    // 修改密码
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if(valid) {
+          console.log('校验成功')
+        }else {
+          return false
+        }
+      })
     }
   }
-};
+}
 </script>
 
 <style scoped>
